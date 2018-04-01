8f1a4f1508f138232077cea123f97958422b77bf
shuvoshaha <shuvoshaha7@gmail.com>
2018-04-01 21:36:07 +0600

    Done except connecting ot data base

--- AlgorithmSimulator.py
+++ AlgorithmSimulator.py
@@ -7,5 +7,19 @@ app = Flask(__name__)
 def hello_world():
     return render_template('mainPage.html')
 
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+@app.route('/showSignIn')
+def showSignIn():
+    return render_template('singin.html')
+
+@app.route('/showTutorial')
+def tutorial():
+    return render_template('tutorial.html')
 if __name__ == '__main__':
-        app.run()
+        app.run(port=5002,debug=True)
+@Qapp.route('/backHome')
+def return_home():
+    return render_template('mainPage.html')
